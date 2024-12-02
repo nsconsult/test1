67166fdd3393e9657f1006cb9e7331a676cb7d16
nsconsult <nazim.ali@epitech.eu>
2024-12-02 15:17:43 +0000

    FIX

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack: (config) => {
         config.resolve.alias['@'] = path.resolve('./src');
         return config;
-      },
-      eslint: {
+    },
+    eslint: {
         ignoreDuringBuilds: true, // Ignore les erreurs ESLint pendant le build
-      },
+    },
+    env: {
+        ROOT_PATH: process.env.ROOT_PATH || '/', // Définir ROOT_PATH avec une valeur par défaut
+    },
 };
-
 
 export default nextConfig;
